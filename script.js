0705f95182a386592bfd0700a1964bb6247ebfde
laterkhaoula <laterkhaoula@gmail.com>
2026-05-22 10:09:12 +0100

    partie javascript_2

--- script.js
+++ script.js
@@ -19,3 +19,34 @@ let currentBook = null;
    ACCUEIL ELEMENTS
 ====================================== */
 
+const genreFilter = document.getElementById("genre-filter");
+const searchInput = document.getElementById("global-search");
+
+const modal = document.getElementById("book-modal");
+const closeModalBtn = document.getElementById("close-modal-btn");
+
+const modalCover = document.getElementById("modal-cover");
+const modalTitle = document.getElementById("modal-title");
+const modalAuthor = document.getElementById("modal-author");
+const modalGenre = document.getElementById("modal-genre");
+const modalDescription = document.getElementById("modal-description");
+const modalToggleBtn = document.getElementById("modal-toggle-alire-btn");
+
+/* ======================================
+   ADMIN ELEMENTS
+====================================== */
+
+const form = document.getElementById("admin-book-form");
+
+const bookIdInput = document.getElementById("book-id");
+
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const genreInput = document.getElementById("genre");
+const coverInput = document.getElementById("cover");
+const descriptionInput = document.getElementById("description");
+
+const formTitle = document.getElementById("form-action-title");
+const submitBtn = document.getElementById("submit-btn");
+const cancelEditBtn = document.getElementById("cancel-edit-btn");
+
